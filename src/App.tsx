1c87a68e94b140f1789d28631b446b46e54b328b
Safin Singh <safin.singh@gmail.com>
2020-07-27 16:48:28 -0700

    Initialize firebase, add light/dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
 import React, { FunctionComponent } from 'react'
 import { Switch, Route } from 'react-router-dom'
-import { Flex } from '@chakra-ui/core'
 
 import Lander from './components/Lander'
 
 const App: FunctionComponent = () => {
   return (
-    <Flex h="100vh" bg="gray.900" align="center" justify="center">
-      <Switch>
-        <Route exact path="/" component={Lander} />
-      </Switch>
-    </Flex>
+    <Switch>
+      <Route exact path="/" component={Lander} />
+    </Switch>
   )
 }
 
--- src/components/Lander.tsx
+++ src/components/Lander.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import {
   Flex,
   Input,
@@ -6,35 +6,51 @@ import {
   Text,
   IconButton,
   Box,
+  Button,
+  useColorMode,
 } from '@chakra-ui/core'
-import { Formik, Field } from 'formik'
+import { Formik, Field, FormikProps } from 'formik'
 import { v4 as uuidv4 } from 'uuid'
 
 interface event {
   [id: string]: { name: string; finished: boolean }
 }
 
-interface state {
+interface eventState {
   events: event
 }
 
-class Lander extends Component {
-  state: state = { events: {} }
+const Lander = (): JSX.Element => {
+  const [state, setState] = useState<eventState>({ events: {} })
+  const { colorMode, toggleColorMode } = useColorMode()
 
-  removeItem = (e: any) => {
-    const newEvents = this.state.events
+  const removeItem = (e: any) => {
+    const newEvents = state.events
     delete newEvents[e.currentTarget.parentElement.id]
 
-    this.setState({
-      ...this.state,
+    setState({
+      ...state,
       events: newEvents,
     })
   }
 
-  render(): JSX.Element {
-    return (
+  return (
+    <Flex
+      h="100vh"
+      bg={colorMode === 'dark' ? 'gray.900' : 'gray.50'}
+      align="center"
+      justify="center"
+    >
       <Flex w="70%" h="100%" justify="center" align="center" direction="column">
-        <Box marginY={6}>
+        <Button
+          onClick={toggleColorMode}
+          position="absolute"
+          top="10px"
+          right="10px"
+        >
+          Toggle {colorMode === 'light' ? 'Dark' : 'Light'}
+        </Button>
+        <Box marginY={4}>
           <Text fontSize="6xl" as="b">
             trck.
           </Text>
@@ -44,16 +60,22 @@ class Lander extends Component {
             <Formik
               initialValues={{ newTodo: '' }}
               onSubmit={(values, { resetForm }) => {
-                this.setState({
-                  events: {
-                    ...this.state.events,
-                    [uuidv4()]: { name: values['newTodo'], finished: false },
-                  },
-                })
-                resetForm()
+                if (values['newTodo']) {
+                  setState({
+                    events: {
+                      ...state.events,
+                      [uuidv4()]: { name: values['newTodo'], finished: false },
+                    },
+                  })
+                  resetForm()
+                }
               }}
             >
-              {(props) => (
+              {(
+                props: FormikProps<{
+                  newTodo: string
+                }>
+              ) => (
                 <form onSubmit={props.handleSubmit} style={{ width: '80%' }}>
                   <Field name="newTodo" w="100%">
                     {({ field }: { field: string }) => (
@@ -66,7 +88,7 @@ class Lander extends Component {
                           w="100%"
                           padding="10px"
                           variant="filled"
-                          bg="gray.800"
+                          bg={colorMode === 'dark' ? 'gray.800' : 'gray.200'}
                           rounded="lg"
                         />
                       </FormControl>
@@ -83,15 +105,15 @@ class Lander extends Component {
             direction="column"
             my={4}
           >
-            {Object.keys(this.state.events).length === 0 ? (
+            {Object.keys(state.events).length === 0 ? (
               <Text my={1}>No events yet, add one to get started!</Text>
             ) : (
-              Object.keys(this.state.events).map((e, index) => (
+              Object.keys(state.events).map((e, index) => (
                 <Flex
                   key={index}
                   id={e}
                   my={1}
-                  bg="gray.800"
+                  bg={colorMode === 'dark' ? 'gray.800' : 'gray.200'}
                   w="80%"
                   padding="10px"
                   rounded="lg"
@@ -105,22 +127,22 @@ class Lander extends Component {
                     opacity={0}
                     cursor="none"
                   />
-                  <Text>{this.state.events[e].name}</Text>
+                  <Text>{state.events[e].name}</Text>
                   <IconButton
                     aria-label="Search database"
                     icon="delete"
                     variant="ghost"
                     size="sm"
-                    onClick={this.removeItem}
+                    onClick={removeItem}
                   />
                 </Flex>
               ))
             )}
           </Flex>
         </Box>
-      </Flex>
-    )
-  }
+      </Flex>{' '}
+    </Flex>
+  )
 }
 
 export default Lander
